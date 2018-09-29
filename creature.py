5897a1d5edc967d563427cb54be0420ea9f44398
William James <34557830+wlpjames@users.noreply.github.com>
2018-09-29 15:56:11 +0100

    Update creature.py

--- creature.py
+++ creature.py
@@ -219,7 +219,7 @@ def fetchFromDB(message):
     #takes a string from the database and returns what the database gives.
 
     # cursor
-    conn_string = "pool.db"
+    conn_string = "postgres://lxlameafsxcwnd:d0f2f7f2a3b0110169d6237dd40097000cdadcef849ef2a719246a46658e6658@ec2-54-217-205-90.eu-west-1.compute.amazonaws.com:5432/d5iu3re523ghb0"
     connection = psycopg2.connect(conn_string)
     cursor.execute(message)
     #print("this is the rowcount")
@@ -233,7 +233,7 @@ def fetchFromDB(message):
 
 def sendToDB(message):
     # cursor
-    conn_string = "pool.db"
+    conn_string = "postgres://lxlameafsxcwnd:d0f2f7f2a3b0110169d6237dd40097000cdadcef849ef2a719246a46658e6658@ec2-54-217-205-90.eu-west-1.compute.amazonaws.com:5432/d5iu3re523ghb0"
     connection = psycopg2.connect(conn_string)
     cursor = connection.cursor()
     cursor.execute(message)
